07482822f9540475a3beb8ef0dd6dbdb5b5c2a03
Joao Gomes <jowgomes3@gmail.com>
2024-12-17 20:03:33 -0300

    feat: add header to Products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,30 @@
-const ProductsPage = () => {
-  return <h1>Products page</h1>;
+// import { db } from "../_lib/prisma";
+
+import { Button } from "../_components/ui/button";
+import { Plus } from "lucide-react";
+
+const ProductsPage = async () => {
+  // const products = await db.product.findMany();
+  return (
+    <div className="w-full space-y-8 p-8">
+      {/* CABEÇALHO */}
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-[#00A180]">Produtos</span>
+          <p className="text-xl font-semibold">Gestão de Produtos</p>
+        </div>
+        <div className="flex items-end">
+          <Button size="sm" className="bg-[#00A180] py-2">
+            <Plus size={20} />
+            Novo produto
+          </Button>
+        </div>
+      </div>
+
+      {/* LISTAGEM DE PRODUTOS */}
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductsPage;
